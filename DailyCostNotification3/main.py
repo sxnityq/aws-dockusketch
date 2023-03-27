0c9c00bc4df9874fa92ad928603e7bf40e83faa4
sxnity <markspet662@gmail.com>
2023-03-27 09:59:59 +0300

    FIX incorrect bills evaluation

--- DailyCostNotification3/main.py
+++ DailyCostNotification3/main.py
@@ -1,12 +1,15 @@
 import json
 from datetime import datetime, timedelta
+import os
 
 import requests
+from dotenv import load_dotenv
 
 from dailyCostNotification import DCN, BillingDays
 from utils import MyJsonEncoder
 
-webhook_url = "https://hooks.slack.com/services/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"
+
+load_dotenv()
 myEncoder = MyJsonEncoder()
 today = datetime.now()
 yesterday = today - timedelta(days=1)
@@ -14,59 +17,62 @@ billing_data = BillingDays()
 
 def collect_prev_month_bills() -> int:
         
-    month_billing = 0
-    for dp in DCN.get_billing_dp(start_time=billing_data.get_prev_month_first_day(),
+    month_bill = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=billing_data.get_prev_month_first_day(),
                                 end_time=billing_data.get_prev_month_last_day(),
-                                period=3600).get("Datapoints"):
-    
-        month_billing = dp.get('Average')
+                                period=3600).get("Datapoints")]
         
-    return month_billing
+    return 0.0 if not month_bill else max(month_bill)
 
 def collect_current_month_bills() -> int:
-
-    month_billing = 0
-    for dp in DCN.get_billing_dp(start_time=billing_data.get_cur_month_first_day(),
+  
+    month_bill = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=billing_data.get_cur_month_first_day(),
                                 end_time=billing_data.get_cur_month_last_day(),
-                                period=3600).get("Datapoints"):
-    
-        month_billing = dp.get('Average')
-        
-    return month_billing
+                                period=3600).get("Datapoints")]
+
+    return 0.0 if not month_bill else max(month_bill)
 
 def get_todays_bills() -> int:
     
-    today_billing = 0
-    for dp in DCN.get_billing_dp(start_time=datetime(today.year, today.month, today.day),
+    today_billing = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=datetime(today.year, today.month, today.day),
                                 end_time=today,
-                                period=3600).get("Datapoints"):
-        today_billing = dp.get('Average')
+                                period=3600).get("Datapoints")]
+    if not today_billing:
+        today_billing = 0.0
+    else:
+        today_billing = max(today_billing)
     
-    yesterday_billing = 0
-    for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day),
+    yesterday_billing = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day),
                                 end_time=datetime(today.year, today.month, today.day),
-                                period=3600).get("Datapoints"):
-        yesterday_billing = dp.get('Average')
+                                period=3600).get("Datapoints")]
+    
+    if not yesterday_billing:
+        yesterday_billing = 0.0
+    else:
+        yesterday_billing = max(yesterday_billing)
         
-    return round(today_billing - yesterday_billing, 2)
+    return today_billing if yesterday_billing > today_billing else round(today_billing - yesterday_billing, 2)
 
 def get_yesterday_bills() -> int:
     
-    yesterday_billing = 0
-    for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day),
+    yesterday_billing = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day),
                                 end_time=datetime(today.year, today.month, today.day),
-                                period=3600).get("Datapoints"):
-
-        yesterday_billing = dp.get('Average')
-        
-    two_days_ago_billing = 0
-    for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day) - timedelta(days=1),
-                                end_time=datetime(yesterday.year, yesterday.month, yesterday.day),
-                                period=3600).get("Datapoints"):
-        
-        two_days_ago_billing = dp.get('Average')
+                                period=3600).get("Datapoints")]
     
-    return round(yesterday_billing - two_days_ago_billing, 2)
+    if not yesterday_billing:
+        yesterday_billing = 0.0
+    else:
+        yesterday_billing = max(yesterday_billing)
+    
+    two_days_ago_billing = [dp.get('Average') for dp in DCN.get_billing_dp(start_time=datetime(yesterday.year, yesterday.month, yesterday.day) - timedelta(days=1),
+                                end_time=datetime(yesterday.year, yesterday.month, yesterday.day),
+                                period=3600).get("Datapoints")]
+    
+    if not two_days_ago_billing:
+        two_days_ago_billing = 0.0
+    else:
+        two_days_ago_billing = max(two_days_ago_billing)
+        
+    return yesterday_billing if two_days_ago_billing > yesterday_billing  else  round(yesterday_billing - two_days_ago_billing, 2)
 
 def send_message():
     pF_month = billing_data.get_prev_month_first_day()
@@ -74,20 +80,17 @@ def send_message():
     cL_month = billing_data.get_cur_month_last_day()
     response = {
            "text" : f"""[TODAY🗓]: {today.year}-{str(today.month).zfill(2)}-{str(today.day).zfill(2)}\
-                    \nExpenses = {get_todays_bills()} USD\
+                    \nExpenses: {get_todays_bills()} USD\
                     \n[YESTERDAY🗓]: {yesterday.year}-{str(yesterday.month).zfill(2)}-{str(yesterday.day).zfill(2)}\
-                    \nExpenses = {get_yesterday_bills()} USD\
+                    \nExpenses: {get_yesterday_bills()} USD\
                     \n[CUR MONTH🗓]: {today.year}-{str(today.month).zfill(2)}-01 to {today.year}-{str(today.month).zfill(2)}-{str(cL_month.day).zfill(2)}\
                     \nExpenses: {collect_current_month_bills()} USD\
                     \n[PREV MONTH🗓]: {pF_month.year}-{str(pF_month.month).zfill(2)}-01 to {pL_month.year}-{str(pL_month.month).zfill(2)}-{str(pL_month.day).zfill(2)}\
                     \nExpenses: {collect_prev_month_bills()} USD
                     """}
-    return response
+    response = json.dumps(response)
+    return requests.post(url=os.getenv("webhook_url"), data=response)
 
-def main():
-    response = json.dumps(send_message())
-    res = requests.post(url=webhook_url, data=response)
-    return res
-
-if __name__ == "__main__":
-    print(main())
+def main(event, context):
+    send_message()
+    return "expense statistics sent successfully"
